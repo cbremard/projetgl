2583efba40690a2527fb7caa2c426df4957e079e
cbremard <corentin.bremard@eleve.ensai.fr>
2013-12-03 18:00:42 +0100

    Changement des classes Etat et début de code de la classe Github.

--- projetGL/src/main/java/projetGL/metier/GoogleSearch.java
+++ projetGL/src/main/java/projetGL/metier/GoogleSearch.java
@@ -1,6 +1,11 @@
 package projetGL.metier;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Stack;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Evaluator.IsEmpty;
 
 public class GoogleSearch extends MethodJunior{
 	private static GoogleSearch uniqueGoogle = null;
@@ -17,8 +22,19 @@ public class GoogleSearch extends MethodJunior{
 		return 0;
 	}
 	
-	public ArrayList<String> getUrlResult(String request){
-		ArrayList<String> urls = new ArrayList();
+	public Stack<String> getUrlResult(String request){
+		Stack<String> urls = new Stack();
+		Document doc = null;
+		System.out.println(request);
+		try {
+			doc = Jsoup.connect(request).userAgent("Firefox").get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (doc != null){
+			urls.add(doc.toString());
+		}
 		//TODO by CorentinB
 		return urls;
 	}
@@ -27,6 +43,11 @@ public class GoogleSearch extends MethodJunior{
 	public float getScore() {
 		// TODO Auto-generated method stub
 		// return GoogleSearch.getInstance().getNbResult("theREquest");
+		return 0;
+	}
+
+	public float compute() {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
