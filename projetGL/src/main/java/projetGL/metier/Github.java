fcea9baa2361be11012c747e6670eec61a1c75ed
cbremard <corentin.bremard@eleve.ensai.fr>
2013-12-15 17:49:52 +0100

    Début de l'implémentation des tests sur la classe Github

--- projetGL/src/main/java/projetGL/metier/Github.java
+++ projetGL/src/main/java/projetGL/metier/Github.java
@@ -24,19 +24,6 @@ public class Github extends Api{
 	float coeff;
 
 
-	public static void main( String[] args ){
-		//    	String url = "https://www.google.fr/search?client=ubuntu&channel=fs&q=java+url+getContent-Length&ie=utf-8&oe=utf-8&gws_rd=cr&ei=a4mtUterMIi20QX84ID4Bw#channel=fs&q=entamer";
-		String url = "https://github.com/cbremard/projetgl/tree/master/projetGL";
-		try {
-			String user = getInstance().getUser(url);
-			System.out.println(user);
-			System.out.println(getInstance().getRepo(url, user));
-		} catch (InvalideGithubUrlException e) {
-			e.printStackTrace();
-		}
-	}
-
-
 	/**
 	 * Constructeur privé de la classe Github pour obliger l'usage de la méthode getInstance
 	 * @author BREMARD Corentin
---
+++ projetGL/src/test/java/projetGL/metier/GithubTest.java
@@ -0,0 +1,14 @@
+package projetGL.metier;
+
+
+public class GithubTest {
+//	String url = "https://www.google.fr/search?client=ubuntu&channel=fs&q=java+url+getContent-Length&ie=utf-8&oe=utf-8&gws_rd=cr&ei=a4mtUterMIi20QX84ID4Bw#channel=fs&q=entamer";
+//	String url = "https://github.com/cbremard/projetgl/tree/master/projetGL";
+//	try {
+//		String user = getInstance().getUser(url);
+//		System.out.println(user);
+//		System.out.println(getInstance().getRepo(url, user));
+//	} catch (InvalideGithubUrlException e) {
+//		e.printStackTrace();
+//	}
+}
