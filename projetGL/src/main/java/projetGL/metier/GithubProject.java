a9700b4bef8136c93c7782998e7b5f5336b61085
fanny-thms <fanny.thomas@eleve.ensai.fr>
2013-12-26 14:07:54 +0100

    Modif de github (bis)

---
+++ projetGL/src/main/java/projetGL/metier/GithubProject.java
@@ -0,0 +1,38 @@
+package projetGL.metier;
+
+
+public class GithubProject {
+
+	private String user;
+	private String repo;
+	private float score;
+	private String comments;
+	
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getRepo() {
+		return repo;
+	}
+	public void setRepo(String repo) {
+		this.repo = repo;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+	
+}
